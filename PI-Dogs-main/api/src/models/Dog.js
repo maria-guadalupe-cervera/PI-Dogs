569dd1ef0f856cec1c1541ed97d4055d22418374
Cervera María Guadalupe <guadacervera8@gmail.com>
2021-09-28 10:17:04 -0300

    models-db

--- PI-Dogs-main/api/src/models/Dog.js
+++ PI-Dogs-main/api/src/models/Dog.js
@@ -3,10 +3,35 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
+  // debe tener id, nombre, altura, peso, años de vida
   sequelize.define('dog', {
+    // Id, va a ser del tipo unique universal id, su valor defaul va a ser un universal unique(v4), no se le va a permitir tener valor en nulo, y va a ser la primary key
+    id:{
+      type:DataTypes.UUID,
+      defaultValue:DataTypes.UUIDV4,
+      allowNull:false,
+      primaryKey:true
+    },
+    // Nombre, va a ser de tipo string y no se le va a permitir ser nulo
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    // altura, va a ser un string (de hecho en la api también esta planteado como string), no se le va a permitir ser nulo
+    height:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    // peso, va a ser de tipo string, no va a poder ser nulo
+    weigth:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    // años vida, string (en la api también es string), no nulo
+    life_span:{
+      type:DataTypes.STRING,
+      allowNull:false
+    }
+  
   });
 };
---
+++ PI-Dogs-main/api/src/utils/dataApiDogs.js
@@ -0,0 +1,44 @@
+const {Dog, Temperament}=require('../db');
+require('dotenv').config(); 
+const axios=require('axios');
+const{URL_API,API_KEY}=process.env
+
+const apiData=async()=>{
+    const api=await axios.get(`${URL_API}?api_key=${API_KEY}`);
+    const apiInfo=await api.data.map(e=>{
+        return{
+            id:e.id,
+            name:e.name,
+            heigth:e.heigth.imperial,
+            weigth:e.weigth.imperial,
+            life_span:e.life_span,
+            image:e.image.url,
+            temperament:e.temperament
+        }
+    });
+    return apiInfo
+}
+const infoDB=async()=>{
+    return await Dog.findAll({
+        include:{
+            model:Temperament,
+            attributes:['name'],
+            through:{
+                attributes:[]
+            }
+        }
+    });
+};
+
+const allDogs=async()=>{
+    const apiInfo=await apiData();
+    const dbInfo=await infoDB();
+    const totalInfo=apiInfo.concat(dbInfo);
+    return totalInfo;
+};
+
+module.exports={
+    apiData,
+    infoDB,
+    allDogs
+}
