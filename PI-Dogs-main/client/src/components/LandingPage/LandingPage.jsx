cdf1ec37a8ed1fb55e9bd17749fc21c07e3b5de4
Cervera María Guadalupe <guadacervera8@gmail.com>
2021-10-01 15:47:53 -0300

    landing

--- PI-Dogs-main/client/src/components/LandingPage/LandingPage.jsx
+++ PI-Dogs-main/client/src/components/LandingPage/LandingPage.jsx
@@ -1,18 +1,27 @@
-import React from "react";
-import{NavLink}from'react-router-dom';
-import style from'./LandingPage.module.css';
+import React from 'react'; 
+import { NavLink } from 'react-router-dom'; 
+import style from './LandingPage.module.css';
+import video from '../../media/video-bg.mp4'
 
-const LandingPage=()=>{
-    return(
-        <div className={style.bar}>
-            <h1>Doogle</h1>
-            <NavLink to='/home'>
-                <button className={style.btn}>
-                    Home
-                </button>
+
+
+
+const LandingPage = () => { 
+    return (
+        <div className={style.container}>
+         <video autoPlay loop muted >
+            <source src={video} type='video/mp4'/>
+         </video>
+        <div className = {style.bar}>
+            <h1>DOoGLE</h1>
+            <NavLink to ='/home'>
+                <button className={style.btn}>Home</button>
             </NavLink>
         </div>
+        
+        </div>
     )
-}
+}; 
+
 
 export default LandingPage;
