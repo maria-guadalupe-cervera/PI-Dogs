0bdcc4120070cedb75d8e99241c0beee29056bb4
Cervera María Guadalupe <guadacervera8@gmail.com>
2021-10-13 14:38:28 -0300

    almost there

--- PI-Dogs-main/client/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const GET_DOGS = 'GET_DOGS';
-export const FILTER_DOGS_CREATED = 'FILTER_DOGS_CREATED';
-export const GET_NAME_DOGS = 'GET_NAME_DOGS';
-export const ADD_DOG = 'ADD_DOG';
-export const GET_TEMPERAMENTS = 'GET_TEMPERAMENTS';
-export const FILTER_TEMP = 'FILTER_TEMP';
-export const ORDER = 'ORDER';
-export const ORDER_WEIGHT = 'ORDER_WEIGHT';
-export const DOG_DETAIL = 'DOG_DETAIL';
